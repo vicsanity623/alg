94348e99e217311d6d228ef27cf4633a88aeeb99
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-02-17 01:01:56 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const GAME_VERSION = 'v5.8'; // Incremented version
+const GAME_VERSION = 'v5.9'; // Incremented version
 const CACHE_NAME = `AIC-${GAME_VERSION}`;
 
 // Added manifest.json to the assets list
